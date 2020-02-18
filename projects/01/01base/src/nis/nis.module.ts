4e198c3b6d68f989eae484a93fe39064e0871b98
soniapini <me@soniapini.info>
2020-02-18 15:46:49 +0100

    fix(01base): eliminato componente mail-logo dal modulo

--- projects/01/01base/src/nis/nis.module.ts
+++ projects/01/01base/src/nis/nis.module.ts
@@ -1,14 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { NisComponent } from './nis.component';
-import { MailLogoComponent } from './components/mail-logo/mail-logo.component';
+import {NisComponent} from './nis.component';
 import {MailMessageService} from './services/mail-message.service';
 
 @NgModule({
   declarations: [
-    NisComponent,
-    MailLogoComponent
+    NisComponent
   ],
   imports: [
     BrowserModule
