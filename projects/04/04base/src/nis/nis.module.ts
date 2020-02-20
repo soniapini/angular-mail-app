bc982401925367267595969929be63e9690f5c94
soniapini <me@soniapini.info>
2020-02-20 15:24:39 +0100

    fix(04base): eliminato il nuovo componente dal modulo

--- projects/04/04base/src/nis/nis.module.ts
+++ projects/04/04base/src/nis/nis.module.ts
@@ -7,15 +7,13 @@ import {MailMessageService} from './services/mail-message.service';
 import {MessageViewerComponent} from './components/message-viewer/message-viewer.component';
 import {FolderListComponent} from './components/folder-list/folder-list.component';
 import {FormsModule} from '@angular/forms';
-import {MessageListComponent} from './components/message-list/message-list.component';
 
 @NgModule({
   declarations: [
     NisMailViewComponent,
     MailLogoComponent,
     MessageViewerComponent,
-    FolderListComponent,
-    MessageListComponent
+    FolderListComponent
   ],
   imports: [
     BrowserModule,
