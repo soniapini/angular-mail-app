02cea4eac0e749c6a50b512128ac3a7a62bc925a
soniapini <me@soniapini.info>
2020-02-20 15:18:53 +0100

    fix(02base): aggiunta implementazione base per Service

--- projects/02/02base/src/nis/services/mail-message.service.ts
+++ projects/02/02base/src/nis/services/mail-message.service.ts
@@ -1,10 +1,47 @@
 import {Injectable} from '@angular/core';
+import {Message} from '../models/message';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MailMessageService {
 
+  messages: Array<Message>;
+
   constructor() {
+    this.messages = [
+      {
+        to: 'carlo.bonamico@gmail.com',
+        from: 'sonia.pini@nispro.it',
+        subject: 'Angular 1.5',
+        body: ' a b c '
+      },
+      {
+        to: 'carlo.bonamico@gmail.com',
+        from: 'carlo.bonamico@nispro.it',
+        subject: 'Typescript',
+        body: ' a b c d e f  '
+      },
+      {
+        to: 'carlo.bonamico@gmail.com',
+        from: 'sonia.pini@nispro.it',
+        subject: 'Flexbox how-to',
+        body: ' a b c d e f  '
+      },
+      {
+        to: 'carlo.bonamico@gmail.com',
+        from: 'sonia.pini@nispro.it',
+        subject: 'Re: ES6 tutorial',
+        body: ' a b c d e f  '
+      }
+    ];
+  }
+
+  getMessages() {
+    return this.messages;
+  }
+
+  getMessagesByFolder(folderName: string): Array<Message> {
+    return null;
   }
 }
