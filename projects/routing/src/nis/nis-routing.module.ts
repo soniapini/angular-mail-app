ef49ddf8ba86319b7e97dcb62bb2c35676fc4cbd
soniapini <me@soniapini.info>
2020-03-02 13:35:27 +0100

    prove di routing multi slot

--- projects/routing/src/nis/nis-routing.module.ts
+++ projects/routing/src/nis/nis-routing.module.ts
@@ -1,12 +1,20 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {MessageListComponent} from './components/message-list/message-list.component';
 import {MessageDetailComponent} from './components/message-detail/message-detail.component';
 import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
 import {MessageListPageComponent} from './components/message-list-page/message-list-page.component';
+import {MailLogoComponent} from './components/mail-logo/mail-logo.component';
 
 const routes: Routes = [
-  {path: 'messages', component: MessageListComponent},
+  {
+    path: 'messages',
+    component: MailLogoComponent,
+    outlet: 'pubblicita'
+  },
+  {
+    path: 'messages',
+    component: MessageListPageComponent
+  },
   {path: 'message/:id', component: MessageDetailComponent},
   {
     path: 'inbox',
@@ -14,9 +22,15 @@ const routes: Routes = [
     data: {title: 'Inbox', folder: 'inbox'}
   },
   {
+    path: 'inbox',
+    component: PageNotFoundComponent,
+    outlet: 'pubblicita',
+    data: {title: 'Inbox', folder: 'inbox'}
+  },
+  {
     path: '',
-    redirectTo: '/inbox',
-    pathMatch: 'full'
+    redirectTo: '/inbox(pubblicita:inbox)',
+    pathMatch: 'full',
   },
   {path: '**', component: PageNotFoundComponent}];
 
